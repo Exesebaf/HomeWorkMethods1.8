e3e2c098e9b6b425dbe6f7edaf0dc5a777337d7f
AleksandrVavilov <vavilov3388@gmail.com>
2022-09-05 19:19:36 +0300

    задания с 1 по 3 по методам с изменениями по требованию наставника.

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,31 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int numberYears = 2003;
-        int total = task1(numberYears);
 
-        int clientDeviseYear = 2021;
-        int clietnOC = 0;
-        int totalOcAndYear = task2(clietnOC,clientDeviseYear);
+        int year = 2004;
+        task1(year);
+
+        int clientDeviseYear = 2020;
+        int clietnOC = 1;
+        task2(clietnOC, clientDeviseYear);
 
         int  deliveryDistance = 95;
-        int totalDays = task3(deliveryDistance);
+        System.out.println("Потребуеться дней на доставку " + task3(deliveryDistance) + " дня");
     }
-    public static int task1( int year){
-
+    public static void task1( int year){
         if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)){
             System.out.println(year + " Год является висококосным");
         } else {
             System.out.println(year + " Год не является висококосным");
-        }return year;
+        }
     }
 
-    public static int task2(int clietnOC, int clientDeviseYear) {
+    public static void task2(int clietnOC, int clientDeviseYear) {
         int currentYear = LocalDate.now().getYear();
         if (clietnOC == 0) {
             if (clientDeviseYear < currentYear) {
                 System.out.println("Установите облегченную версию IOS по ссылке");
-                return clietnOC;
+
             } else {
                 System.out.println("Установите версию приложения IOS по ссылке");
             }
@@ -39,7 +39,7 @@ public class Main {
                 System.out.println("Установите версию приложения Android по ссылке");
             }
         }
-        return clientDeviseYear;
+
     }
     public static int task3( int deliveryDistance){
         int deloveryDays = 1;
@@ -49,8 +49,6 @@ public class Main {
         if (deliveryDistance > 60){
             deloveryDays++;
         }
-        System.out.println("Потребуеться дней на доставку " +deloveryDays + " дня");
-
         return deloveryDays;
     }
 
